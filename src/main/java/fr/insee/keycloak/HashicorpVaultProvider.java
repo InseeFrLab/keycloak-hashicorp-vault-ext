ee4a1f1648062d073eac955d31b956ae1af6e890
Cecile Chemin <cecile.chemin@insee.fr>
2019-11-26 12:33:47 +0000

    Resolve "key version management"

--- src/main/java/fr/insee/keycloak/HashicorpVaultProvider.java
+++ src/main/java/fr/insee/keycloak/HashicorpVaultProvider.java
@@ -2,6 +2,7 @@ package fr.insee.keycloak;
 
 import java.util.Optional;
 
+import org.jboss.logging.Logger;
 import org.keycloak.vault.DefaultVaultRawSecret;
 import org.keycloak.vault.VaultProvider;
 import org.keycloak.vault.VaultRawSecret;
@@ -12,20 +13,30 @@ import fr.insee.vault.VaultService;
  * HashicorpVaultProviderFactory
  */
 public class HashicorpVaultProvider implements VaultProvider {
+   private static final Logger logger = Logger.getLogger(HashicorpVaultProviderFactory.class);
+
    private String vaultUrl;
    private String vaultToken;
    private String realmName;
    private String vaultSecretEngineName;
    private VaultService service;
 
-   
-
    @Override
    public VaultRawSecret obtainSecret(String vaultSecretId) {
+      int secretVersion = 0;
+      String vaultSecretName = vaultSecretId;
+      if (vaultSecretId.contains(":")) {
+         try {
+            secretVersion = Integer.parseInt(vaultSecretId.substring(vaultSecretId.lastIndexOf(":")));
+            vaultSecretName = vaultSecretId.substring(0, vaultSecretId.lastIndexOf(":"));
+         } catch (NumberFormatException e) {
+            logger.error("last string after : is expected to be the version number");
+         }
+      }
+
      return DefaultVaultRawSecret.forBuffer(
          Optional.of(
-            service.getSecretFromVault(vaultUrl, realmName, vaultSecretEngineName, vaultSecretId, vaultToken)));
-      
+            service.getSecretFromVault(vaultUrl, realmName, vaultSecretEngineName, vaultSecretName, vaultToken, secretVersion)));
    }
 
    @Override
